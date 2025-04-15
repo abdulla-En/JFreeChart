ebc8a11196b402be07ca4013a1d788a5bea5f52a
abdulla <abd.ulla4112018@gmail.com>
2025-04-15 16:19:11 +0200

    Intial commit

---
+++ src/main/java/com/Jfree/DiscountManager.java
@@ -0,0 +1,23 @@
+package com.Jfree;
+
+
+public class DiscountManager {
+    boolean isDiscountsSeason;
+    IDiscountCalculator discountCalculator;
+
+    public DiscountManager(boolean isDiscountsSeason, IDiscountCalculator discountCalculator) {
+        this.isDiscountsSeason = isDiscountsSeason;
+        this.discountCalculator = discountCalculator;
+    }
+
+    public double calculatePriceAfterDiscount(double price) {
+        if (! isDiscountsSeason) {
+            return price;
+        }
+
+        if (discountCalculator.isTheSpecialWeek())
+            return price * .8;
+
+        return price * discountCalculator.getDiscountPercentage();
+    }
+}
---
+++ src/main/java/com/Jfree/IDiscountCalculator.java
@@ -0,0 +1,6 @@
+package com.Jfree;
+
+public interface IDiscountCalculator {
+    boolean isTheSpecialWeek();
+    int getDiscountPercentage();
+}
